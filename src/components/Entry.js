4008fa41a31194d146f9ea7174c760500e66fa2d
Steev Sachs <steev.sachs@gmail.com>
2018-10-05 13:48:11 -0400

    Add Run in GraphiQL links to entries

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import Request from './Request';
+import Definition from './Definition';
+
+const makeGraphiQlUrl = (entry) => {
+  const base = entry.url;
+  const query = encodeURIComponent(entry.bareQuery);
+  const variables = encodeURIComponent(JSON.stringify(entry.queryVariables));
+  return `${base}/graphiql?query=${query}&variables=${variables}`;
+};
 
 export default function Entry({
   entry,
@@ -13,12 +20,15 @@ export default function Entry({
       </div>
       {entry.data && entry.data.map((request, i) => {
         if (request.kind !== 'FragmentDefinition') {
-          return <Request key={`request-${i}`} request={request} />;
+          return <Definition key={`request-${i}`} request={request} />;
         }
       })}
       {!entry.data && (
         <p className="error">{entry}</p>
       )}
+      <div className="runInGraphiQl">
+        <a target="_blank" href={makeGraphiQlUrl(entry)}>Run in GraphiQL</a>
+      </div>
     </div>
   );
 }
